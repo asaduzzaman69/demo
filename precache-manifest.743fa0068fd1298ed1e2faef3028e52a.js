341906e0a8c5983fe8253c9f3ceeeda13b5ccddf
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2020-07-17 15:50:56 +0600

    Updates

---
+++ precache-manifest.743fa0068fd1298ed1e2faef3028e52a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511b9e45f881e597c6843e49ba56e667",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "0559cb61003720c69509",
+    "url": "/demo/static/css/main.f4d0c699.chunk.css"
+  },
+  {
+    "revision": "8fae0d216e3a73a97c19",
+    "url": "/demo/static/js/2.de61eac6.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/demo/static/js/2.de61eac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0559cb61003720c69509",
+    "url": "/demo/static/js/main.fe772cb9.chunk.js"
+  },
+  {
+    "revision": "514982369ecb08635522",
+    "url": "/demo/static/js/runtime-main.13453bf2.js"
+  }
+]);
